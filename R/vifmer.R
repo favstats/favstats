4086fd727448acedd115ae01c6ec71b7517ef37f
Fabio Votta <fabio.votta@gmail.com>
2017-11-03 00:46:29 +0100

    vifmer

---
+++ R/vifmer.R
@@ -0,0 +1,27 @@
+#' VIF for Multilevel
+#'
+#' This function allows you to calculate VIFs for multilevel modells.
+#' @param
+#' @keywords VIF
+#' @export
+#' @examples
+#' vif.mer()
+
+vif.mer <- function (fit) {
+  ## adapted from rms::vif
+  
+  v <- vcov(fit)
+  nam <- names(fixef(fit))
+  
+  ## exclude intercepts
+  ns <- sum(1 * (nam == "Intercept" | nam == "(Intercept)"))
+  if (ns > 0) {
+    v <- v[-(1:ns), -(1:ns), drop = FALSE]
+    nam <- nam[-(1:ns)]
+  }
+  
+  d <- diag(v)^0.5
+  v <- diag(solve(v/(d %o% d)))
+  names(v) <- nam
+  v
+}
