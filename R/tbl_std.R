46f9debbd77008cd558d1941402287d644aa3f23
Fabio Votta <fabio.votta@gmail.com>
2017-11-06 23:15:07 +0100

    ss

--- R/tbl_std.R
+++ R/tbl_std.R
@@ -1,12 +1,12 @@
 #' Create table with standardized estimates
 #'
-#' This function creates a stargazer table 
+#' This function creates a stargazer table
 #' with standardized estimates
 #'
 #' @param model put in the model (lm object) here.
 #' @param ... stargazer options.
 #'
-#' @return 
+#' @return returns a stargazer table.
 #' @examples
 #' # #fake data
 #' var1<-rnorm(100, mean=10, sd=5)
@@ -16,7 +16,7 @@
 #' df<-data.frame(var1, var2, var3, var4)
 #' #model with unstandardized betas
 #' model1<-lm(var1~var2+var3+var4, data=df)
-#' 
+#'
 #' tbl_std(model1, type = "text")
 
 
@@ -26,10 +26,10 @@ tbl_std <- function(model, ...) {
   #print
   model1.beta$standardized.coefficients[1] <- NA
   #coef(summary(model1.beta))[, "Std. Error"]  <- NA
-  
-  stargazer(model, model1.beta, 
-            coef = list(model$coefficients, 
-                        model1.beta$standardized.coefficients), 
+
+  stargazer(model, model1.beta,
+            coef = list(model$coefficients,
+                        model1.beta$standardized.coefficients),
             column.labels = c("b", "b*"),
             ...)
 }
